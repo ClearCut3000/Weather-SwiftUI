9899e304c5d7f1ebd0952c09de6aeb38be411d11
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-19 23:14:04 +0300

    Initial commit

--- Weather-SwiftUI/ContentView.swift
+++ Weather-SwiftUI/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+  //MARK: - Main View
+  var body: some View {
+    ZStack {
+      LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                     startPoint: .topLeading,
+                     endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
